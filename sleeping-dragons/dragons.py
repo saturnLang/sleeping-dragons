3b3cdacec779d2ce749c29a2eb835a12170b45be
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-17 13:02:13 -0400

    Update dragons.py

--- sleeping-dragons/dragons.py
+++ sleeping-dragons/dragons.py
@@ -57,7 +57,7 @@ def draw():
        screen.draw.text("YOU DIED!", fontsize=60, center=CENTER, color=FONT_COLOR)
    elif game_complete:
        screen.draw.text("YOU WON!", fontsize=60, center=CENTER, color=FONT_COLOR)
-       import mission2
+       import missiontwo
    else:
         hero.draw()
         draw_lairs(lairs)
